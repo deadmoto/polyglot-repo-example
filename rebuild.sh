ef01d97d75cbf06e073cd8b0e3e804ade61e8ecd
Dmitry Berezhnoy <dberezhnoy@snapchat.com>
2020-03-18 21:47:49 -0700

    Add bash script to rebuild targets affected by changed files (relative to origin/master).

---
+++ rebuild.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+base=$(git merge-base origin/master HEAD)
+echo Merge base:
+echo - ${base}
+echo
+
+declare -a files=($(git diff --name-only HEAD ${base}))
+if [[ ${#files[@]} -eq 0 ]]; then
+    echo No changed files
+    exit 0
+fi
+echo Changed files:
+printf " - %s\n" ${files[@]}
+echo
+
+declare -a targets=($(bazel query "rdeps(//...,set(${files[@]}))" 2>/dev/null))
+if [[ ${#targets[@]} -eq 0 ]]; then
+    echo No changed targets
+    exit 0
+fi
+echo Affected targets:
+printf " - %s\n" ${targets[@]}
+echo
+
+echo Rebuild targets: bazel build ${targets[@]}
+bazel build ${targets[@]}
